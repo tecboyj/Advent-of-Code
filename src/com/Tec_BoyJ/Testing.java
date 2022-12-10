87dd6781def88a8c9f72a930f75fe7a075029acf
tecboyj <108207813+tecboyj@users.noreply.github.com>
2022-12-09 21:39:21 -0500

    Delete Testing.java

--- src/com/Tec_BoyJ/Testing.java
+++
@@ -1,8 +0,0 @@
-package com.Tec_BoyJ;
-
-public class Testing {
-    public static void main(String[] args) {
-        String a = "1234567890";
-        System.out.println(a.substring(0, 4));
-    }
-}
