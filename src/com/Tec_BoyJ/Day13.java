97f1886f31c15a967b471085870db77fe2524b05
tecboyj <jc515081@gmail.com>
2022-12-13 13:54:10 -0500

    Advent of Code

--- src/com/Tec_BoyJ/Day13.java
+++ src/com/Tec_BoyJ/Day13.java
@@ -10,9 +10,9 @@ import static com.Tec_BoyJ.Main.Main.*;
 public class Day13 {
     public static void main(String[] args) throws URISyntaxException, IOException {
         Main main = new Main("/Tec_BoyJ/Day13.txt", "/Tec_BoyJ/Day13 Practice.txt", "/Others/Day13.txt");
-
         for (int f = 0; f < main.file.length; f++) {
             String[] arr = main.setUp(f, main.file[f]);
+
             System.out.println(ANSI_GREEN + "Part 1: --------------------" + ANSI_RESET);
             System.out.println(ANSI_GREEN + "Part 2: --------------------" + ANSI_RESET);
         }
